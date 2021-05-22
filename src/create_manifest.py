a0a8cbf313c21fabd35e1e86c4b761fae1c5da51
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-05-22 14:35:04 -0400

    feat(`create_manifest.py`): add script to extract models for Majestic manuals

--- src/create_manifest.py
+++ src/create_manifest.py
@@ -1,3 +1,7 @@
+# __Author__: Khoi Van 2021
+
+import argparse
+
 import sys
 import logging
 import re
@@ -34,60 +38,85 @@ INPUT_FOLDER = DATA_FOLDER / 'manuals'
 RESULT_FILE = DATA_FOLDER / 'manifest.csv'
 
 
-def create_manifest_from_manuals(files, result_file):
-    # # Synchronouse fashion, easy for debug
-    # for file in files:
-    #     try:
-    #         extract_sku(file=file, result_file=result_file)
-    #     except Exception as error:
-    #         log.error(f'{file=}')
-    #         log.exception(error)
-
-    # Asynchronous fashion, faster
-    # Ref: https://github.com/willmcgugan/rich/issues/121
-    progress = Progress(SpinnerColumn(),
-                        "[bold green]{task.description}",
-                        BarColumn(),
-                        "[progress.percentage]{task.percentage:>3.1f}%",
-                        "({task.completed} of {task.total})"
-                        "•",
-                        TimeElapsedColumn(),
-                        # transient=True,
-                        console=console)
-
-    with progress:
-        progress.log(f'Extracting model numbers from Installation Manuals')
-        task_description = f'Extracting ...'
-        task_id = progress.add_task(task_description, total=len(files), start=True)
-
-        # for image_file in files:
-        #     extract_sku(image_file)
-        #     progress.advance(task_id)
-
-        try:
-            pool_size = 25
-            with Pool(processes=pool_size) as p:
-                results = p.imap(partial(extract_sku,
-                                         result_file=result_file,
-                                        ),
-                                 files,
-                                 chunksize=8)
-                for result in results:
-                    progress.advance(task_id)
-
-        except Exception as error:
-            # if debug:
-            # traceback_str = ''.join(traceback.format_exception(etype=type(error), value=error, tb=error.__traceback__))
-            # log.error(traceback_str)
-            # log.exception(error)
-            # console.log(f'{file}')
-            console.log(error)
+def init_argparse() -> argparse.ArgumentParser:
+    """Creating CLI helper"""
+    parser = argparse.ArgumentParser(
+        usage="python %(prog)s [OPTIONS]",
+        description="Validate North Country Fire data for Napoleon dataset."
+    )
+    parser.add_argument('-d', '--debug',
+                        help='Print more debug info.',
+                        action="store_true")
+    parser.add_argument('-p', '--parallel',
+                        help='Reading PDF files in asynchronous fashion (default).',
+                        default=True,
+                        action="store_true")
+    parser.add_argument('-s', '--sequential',
+                        help='Reading PDF files sequentially.',
+                        action="store_true")
+    return parser
 
 
-def extract_sku(file, result_file):
+def create_manifest_from_manuals(files,
+                                 result_file,
+                                 parsing_mode: str,
+                                 debug: bool = False):
+    # Synchronouse fashion, easy for debug
+    if parsing_mode == 'sequential':
+        for file in files:
+            try:
+                extract_sku(file=file, result_file=result_file, debug=debug)
+            except Exception as error:
+                log.error(f'{file=}')
+                log.exception(error)
+
+    else:
+        # Asynchronous fashion, faster
+        # Ref: https://github.com/willmcgugan/rich/issues/121
+        progress = Progress(SpinnerColumn(),
+                            "[bold green]{task.description}",
+                            BarColumn(),
+                            "[progress.percentage]{task.percentage:>3.1f}%",
+                            "({task.completed} of {task.total})"
+                            "•",
+                            TimeElapsedColumn(),
+                            # transient=True,
+                            console=console)
+
+        with progress:
+            progress.log(f'Extracting model numbers from Installation Manuals')
+            task_description = f'Extracting ...'
+            task_id = progress.add_task(task_description, total=len(files), start=True)
+
+            # for image_file in files:
+            #     extract_sku(image_file)
+            #     progress.advance(task_id)
+
+            try:
+                pool_size = 25
+                with Pool(processes=pool_size) as p:
+                    results = p.imap(partial(extract_sku,
+                                             result_file=result_file,
+                                             debug=debug,
+                                             ),
+                                     files,
+                                     chunksize=8)
+                    for result in results:
+                        progress.advance(task_id)
+
+            except Exception as error:
+                # if debug:
+                # traceback_str = ''.join(traceback.format_exception(etype=type(error), value=error, tb=error.__traceback__))
+                # log.error(traceback_str)
+                log.exception(error)
+                # console.log(f'{file}')
+                # console.log(error)
+
+
+def extract_sku(file, result_file, debug: bool = False):
     try:
         brand = file.parent.name
-        result = extract_sku_from_brand(brand=brand, file=file)
+        result = extract_sku_from_brand(brand=brand, file=file, debug=debug)
         # console.log(f'{result=}')
         write_items_to_csv(file=result_file, lines=result)
     except Exception as error:
@@ -113,16 +142,21 @@ def write_items_to_csv(file, lines):
             writer.writerow(line)
 
 
-def extract_sku_from_brand(brand: str, file: PurePath
+def extract_sku_from_brand(brand: str,
+                           file: PurePath,
+                           debug: bool = False
                            ) -> List[Dict[str, str]]:
     brand_dict = {
         'Dimplex': extract_sku_from_dimplex_manuals,
         'Empire': extract_sku_from_empire_manuals,
+        'Majestic': extract_sku_from_majestic_manuals,
     }
-    return brand_dict[brand](brand=brand, file=file)
+    return brand_dict[brand](brand=brand, file=file, debug=debug)
 
 
-def extract_sku_from_dimplex_manuals(brand: str, file: PurePath
+def extract_sku_from_dimplex_manuals(brand: str,
+                                     file: PurePath,
+                                     debug: bool = False
                                      ) -> List[Dict[str, str]]:
     result = []
     laparams = LAParams(
@@ -153,6 +187,7 @@ def extract_sku_from_dimplex_manuals(brand: str, file: PurePath
                 result.extend([{'sku': sku.split(' ')[0],
                                 'brand': brand,
                                 'pdf_name': file.name,
+                                'manual_type': '',
                                 'pdf_location': str(file.relative_to(INPUT_FOLDER))}
                               for sku in models])
                 # console.log(f'{filename=}')
@@ -160,7 +195,9 @@ def extract_sku_from_dimplex_manuals(brand: str, file: PurePath
     return result
 
 
-def extract_sku_from_empire_manuals(brand: str, file: PurePath
+def extract_sku_from_empire_manuals(brand: str,
+                                    file: PurePath,
+                                    debug: bool = False
                                     ) -> List[Dict[str, str]]:
     result = []
     laparams = LAParams(
@@ -229,6 +266,7 @@ def extract_sku_from_empire_manuals(brand: str, file: PurePath
                 result.extend([{'sku': sku.split(' ')[0],
                                 'brand': brand,
                                 'pdf_name': file.name,
+                                'manual_type': '',
                                 'pdf_location': str(file.relative_to(INPUT_FOLDER))}
                               for sku in expanded_models])
                 # console.log(f'{filename=}')
@@ -252,12 +290,12 @@ def expand_models(models: List[str]) -> List[str]:
     matches = []
     for model in models:
         match = re_variant.findall(model)
-        containing_letters_and_digits = re.search(r'[a-z]+\d+', model, flags=re.IGNORECASE)
+        # containing_letters_and_digits = re.search(r'[a-z]+\d+', model, flags=re.IGNORECASE)
         # breakpoint()
         if match:
             matches.append(match)
         # Check if it is not just letter and contains number(SKU has letters and numbers, at least for 'Empire')
-        elif not model.encode().isalpha() and containing_letters_and_digits:
+        elif is_likely_sku(text=model):
             all_models.append(model)
     for match in matches:
         # To prevent some false alarm such as 'MULTIFUNCTION REMOTE (MF)'
@@ -267,6 +305,20 @@ def expand_models(models: List[str]) -> List[str]:
             all_models.extend(match_combinations)
 
     return all_models
+
+
+def is_likely_sku(text: str) -> bool:
+    # Check if it is not just letter and contains number(SKU has letters and numbers, at least for 'Empire')
+
+    # Exception:
+    exceptions = ['warmmajic-ii']
+    if text.lower() in exceptions:
+        return True
+
+    containing_letters_and_digits = re.search(r'[a-z]+\-*\d+',
+                                              text,
+                                              flags=re.IGNORECASE)
+    return not text.encode().isalpha() and containing_letters_and_digits
 
 
 def get_all_combinations(match: Tuple[str]):
@@ -314,13 +366,92 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 
+def extract_sku_from_majestic_manuals(brand: str,
+                                      file: PurePath,
+                                      debug: bool = False
+                                      ) -> List[Dict[str, str]]:
+    result = []
+
+    # Get type of manuals: 'installation' or 'owner'
+    manual_type = []
+
+    check_next_element = False
+
+    laparams = LAParams(
+        line_margin=0.63,   # Some files such as 'Dimplex/XLF100_Dimplex.pdf' has models number far apart
+        # boxes_flow=1,
+    )
+    pages = extract_pages(file,
+                          page_numbers=[0],
+                          maxpages=1,
+                          laparams=laparams
+                          )
+    for page_layout in pages:
+        for element in page_layout:
+            # # !DEBUG
+            if debug and isinstance(element, LTTextBoxHorizontal):
+                console.log(element)
+                console.log(element.get_text())
+
+            if (isinstance(element, LTTextBoxHorizontal)
+                and 'manual' in element.get_text().lower()):
+                type = re.search(r'install\w+|owner',
+                                        element.get_text().lower(),
+                                        flags=re.IGNORECASE)
+                if type:
+                    manual_type.append(type[0])
+
+            if (isinstance(element, LTTextBoxHorizontal)
+                and ('model' in element.get_text().lower()
+                     or check_next_element)
+                ):
+                # breakpoint()
+                text = element.get_text()
+                # _, _, models = text.partition('\n')
+                # if ':' in text:
+                #     _, _, models = text.partition(':')
+
+                # Sometimes 'Model' is found in the middle of an element, in that case, split there
+                # Sometimes, there are two 'models:'
+                if 'model' in element.get_text().lower():
+                    _, *models = re.split(r'model\(?s?\)?:?', text, flags=re.IGNORECASE)
+                else:
+                    models = re.split(r'model\(?s?\)?:?', text, flags=re.IGNORECASE)
+                models = re.split(r',\s|\n|\s+', ''.join(models).strip())
+                result.extend([{'sku': sku.split(' ')[0],
+                                'brand': brand,
+                                'pdf_name': file.name,
+                                'manual_type': manual_type[0] if manual_type else '',
+                                'pdf_location': str(file.relative_to(INPUT_FOLDER))}
+                              for sku in models
+                              if sku and is_likely_sku(text=sku)])
+                # console.log(f'{filename=}')
+                # console.log(f'{models=}')
+
+                # Signal the program to check the next pdf text element
+                # because sometimes, the series are not recognized to be in
+                # the same box as the one containing 'model'
+                check_next_element = 'model' in element.get_text().lower()
+    # console.log(f'{result=}')
+    return result
+
+
+
 if __name__ == '__main__':
     # Remove the result file if exists
     RESULT_FILE.unlink(missing_ok=True)
 
+    parser = init_argparse()
+    debug = parser.parse_args().debug
+    parallel = parser.parse_args().parallel
+    sequential = parser.parse_args().sequential
+    parsing_mode = 'sequential' if sequential else 'parallel'
+
     # files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/*.pdf')}
-    files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/Empire/*.pdf')}
-    # files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/Empire/DVP36FP3579.pdf')}
-    # files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/Empire/DVCD323642FP3701NP-4-Homeowners.pdf')}
+    files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/Majestic/*.pdf')}
+    # files = {f.resolve() for f in Path(INPUT_FOLDER).glob('**/Majestic/VDY18,24,30 - DUZY.pdf')}
     # breakpoint()
-    create_manifest_from_manuals(files=files, result_file=RESULT_FILE)
+    create_manifest_from_manuals(files=files,
+                                 result_file=RESULT_FILE,
+                                 parsing_mode=parsing_mode,
+                                 debug=debug)
